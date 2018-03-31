99e95a168e407c402129d23c026ee4a5e69aaf4d
Marcos Collado <marquitos.mcm@gmail.com>
2018-03-31 03:31:03 +0200

    Actualizaciones del controlador:  * Va a por el dinero  * Busca en cada agujero si hay algún paquete azul. Lo recoge, lo lleva al destino y apaga la luz azul. Tiene algunos errores

--- experiments/iri1exp.h
+++ experiments/iri1exp.h
@@ -17,55 +17,55 @@ public:
 protected:
     // Overwritten from the superclasses:
     CArena* CreateArena();
-		void AddActuators(CEpuck* pc_epuck);
+	void AddActuators(CEpuck* pc_epuck);
     void AddSensors(CEpuck* pc_epuck);
     void SetController(CEpuck* pc_epuck);
     void CreateAndAddEpucks(CSimulator* pc_simulator);
    	void Reset(); 
 private:
 
-		/* VARIABLES*/
-		/* Extra */
-		int m_nRobotsNumber;
-		int m_nWriteToFile;
-		dVector2* m_pcvRobotPositions;
-		double* m_fRobotOrientations;
-		int m_nRunTime;
+	/* VARIABLES*/
+	/* Extra */
+	int m_nRobotsNumber;
+	int m_nWriteToFile;
+	dVector2* m_pcvRobotPositions;
+	double* m_fRobotOrientations;
+	int m_nRunTime;
 
-		/* Environment */
-		int m_nLightObjectNumber;
-		dVector2 *m_pcvLightObjects;
-		
-		int m_nBlueLightObjectNumber;
-		dVector2 *m_pcvBlueLightObjects;
-		
-		int m_nRedLightObjectNumber;
-		dVector2 *m_pcvRedLightObjects;
-		
-		int m_nNumberOfGroundArea;
-		dVector2* m_vGroundAreaCenter;
-		double* m_fGroundAreaExternalRadius;
-		double * m_fGroundAreaInternalRadius;
-		double * m_fGroundAreaColor;
+	/* Environment */
+	int m_nLightObjectNumber;
+	dVector2 *m_pcvLightObjects;
+	
+	int m_nBlueLightObjectNumber;
+	dVector2 *m_pcvBlueLightObjects;
+	
+	int m_nRedLightObjectNumber;
+	dVector2 *m_pcvRedLightObjects;
+	
+	int m_nNumberOfGroundArea;
+	dVector2* m_vGroundAreaCenter;
+	double* m_fGroundAreaExternalRadius;
+	double * m_fGroundAreaInternalRadius;
+	double * m_fGroundAreaColor;
 
-		/* Morphology */
+	/* Morphology */
 
-		/* Sensors */
-		float m_fLightSensorRange;
-		float m_fBlueLightSensorRange;
-		float m_fRedLightSensorRange;
-		
-		double m_fBatterySensorRange;
-		double m_fBatteryChargeCoef;
-		double m_fBatteryDischargeCoef;
-		
-		double m_fBlueBatterySensorRange;
-		double m_fBlueBatteryChargeCoef;
-		double m_fBlueBatteryDischargeCoef;
-		
-		double m_fRedBatterySensorRange;
-		double m_fRedBatteryChargeCoef;
-		double m_fRedBatteryDischargeCoef;
+	/* Sensors */
+	float m_fLightSensorRange;
+	float m_fBlueLightSensorRange;
+	float m_fRedLightSensorRange;
+	
+	double m_fBatterySensorRange;
+	double m_fBatteryChargeCoef;
+	double m_fBatteryDischargeCoef;
+	
+	double m_fBlueBatterySensorRange;
+	double m_fBlueBatteryChargeCoef;
+	double m_fBlueBatteryDischargeCoef;
+	
+	double m_fRedBatterySensorRange;
+	double m_fRedBatteryChargeCoef;
+	double m_fRedBatteryDischargeCoef;
 		
     double m_fEncoderSensorError;
 };
